dbde91457ccd9f20ac5b738047e30347462ba574
Nadav Hollander <nadav@dharma.io>
2018-05-12 16:38:56 -0700

    fix cdo stub tests

--- test/ts/cdo.ts
+++ test/ts/cdo.ts
@@ -1,4 +1,4 @@
-import {BigNumber} from "bignumber.js";
+import { BigNumber } from "bignumber.js";
 
 import * as ABIDecoder from "abi-decoder";
 import * as chai from "chai";
@@ -8,25 +8,26 @@ import * as Web3 from "web3";
 import * as Units from "./test_utils/units";
 import * as utils from "./test_utils/utils";
 
-import {DebtKernelContract} from "../../types/generated/debt_kernel";
-import {DebtRegistryContract} from "../../types/generated/debt_registry";
-import {DebtTokenContract} from "../../types/generated/debt_token";
-import {DummyTokenContract} from "../../types/generated/dummy_token";
-import {DummyTokenRegistryContract} from "../../types/generated/dummy_token_registry";
-import {RepaymentRouterContract} from "../../types/generated/repayment_router";
-import {SimpleInterestTermsContractContract} from "../../types/generated/simple_interest_terms_contract";
-import {TokenTransferProxyContract} from "../../types/generated/token_transfer_proxy";
+import { DebtKernelContract } from "../../types/generated/debt_kernel";
+import { DebtRegistryContract } from "../../types/generated/debt_registry";
+import { DebtTokenContract } from "../../types/generated/debt_token";
+import { DummyTokenContract } from "../../types/generated/dummy_token";
+import { TokenRegistryContract } from "../../types/generated/token_registry";
+import { RepaymentRouterContract } from "../../types/generated/repayment_router";
+import { SimpleInterestTermsContractContract as SimpleInterestTermsContract } from "../../types/generated/simple_interest_terms_contract";
+import { TokenTransferProxyContract } from "../../types/generated/token_transfer_proxy";
 
-import {DebtKernelErrorCodes} from "../../types/errors";
-import {DebtOrder, SignedDebtOrder} from "../../types/kernel/debt_order";
+import { DebtKernelErrorCodes } from "../../types/errors";
+import { DebtOrder, SignedDebtOrder } from "../../types/kernel/debt_order";
 
-import {BigNumberSetup} from "./test_utils/bignumber_setup";
+import { BigNumberSetup } from "./test_utils/bignumber_setup";
 import ChaiSetup from "./test_utils/chai_setup";
-import {INVALID_OPCODE, REVERT_ERROR} from "./test_utils/constants";
+import { INVALID_OPCODE, REVERT_ERROR } from "./test_utils/constants";
 
-import {DebtOrderFactory} from "./factories/debt_order_factory";
+import { SimpleInterestParameters } from "./factories/terms_contract_parameters";
+import { DebtOrderFactory } from "./factories/debt_order_factory";
 
-import {TxDataPayable} from "../../types/common";
+import { TxDataPayable } from "../../types/common";
 
 import leftPad = require("left-pad");
 
@@ -44,7 +45,7 @@ contract("Collateralized Debt Obligation", async (ACCOUNTS) => {
     let kernel: DebtKernelContract;
     let debtToken: DebtTokenContract;
     let principalToken: DummyTokenContract;
-    let termsContract: SimpleInterestTermsContractContract;
+    let termsContract: SimpleInterestTermsContract;
     let tokenTransferProxy: TokenTransferProxyContract;
 
     let orderFactory: DebtOrderFactory;
@@ -54,20 +55,12 @@ contract("Collateralized Debt Obligation", async (ACCOUNTS) => {
     const DEBTOR_1 = ACCOUNTS[1];
     const DEBTOR_2 = ACCOUNTS[2];
     const DEBTOR_3 = ACCOUNTS[3];
-    const DEBTORS = [
-        DEBTOR_1,
-        DEBTOR_2,
-        DEBTOR_3,
-    ];
+    const DEBTORS = [DEBTOR_1, DEBTOR_2, DEBTOR_3];
 
     const CREDITOR_1 = ACCOUNTS[4];
     const CREDITOR_2 = ACCOUNTS[5];
     const CREDITOR_3 = ACCOUNTS[6];
-    const CREDITORS = [
-        CREDITOR_1,
-        CREDITOR_2,
-        CREDITOR_3,
-    ];
+    const CREDITORS = [CREDITOR_1, CREDITOR_2, CREDITOR_3];
 
     const PAYER = ACCOUNTS[7];
 
@@ -76,58 +69,65 @@ contract("Collateralized Debt Obligation", async (ACCOUNTS) => {
     const TX_DEFAULTS = { from: CONTRACT_OWNER, gas: 4000000 };
 
     before(async () => {
-        const dummyTokenRegistryContract = await DummyTokenRegistryContract.deployed(web3, TX_DEFAULTS);
-        const dummyREPTokenAddress = await dummyTokenRegistryContract.getTokenAddress.callAsync("REP");
+        const dummyTokenRegistryContract = await TokenRegistryContract.deployed(web3, TX_DEFAULTS);
+        const dummyREPTokenAddress = await dummyTokenRegistryContract.getTokenAddressBySymbol.callAsync(
+            "REP",
+        );
+        const dummyREPTokenIndex = await dummyTokenRegistryContract.getTokenIndexBySymbol.callAsync(
+            "REP",
+        );
 
         principalToken = await DummyTokenContract.at(dummyREPTokenAddress, web3, TX_DEFAULTS);
 
         kernel = await DebtKernelContract.deployed(web3, TX_DEFAULTS);
         debtToken = await DebtTokenContract.deployed(web3, TX_DEFAULTS);
         tokenTransferProxy = await TokenTransferProxyContract.deployed(web3, TX_DEFAULTS);
+        termsContract = await SimpleInterestTermsContract.deployed(web3, TX_DEFAULTS);
+        repaymentRouter = await RepaymentRouterContract.deployed(web3, TX_DEFAULTS);
 
         await principalToken.setBalance.sendTransactionAsync(CREDITOR_1, Units.ether(100));
         await principalToken.setBalance.sendTransactionAsync(CREDITOR_2, Units.ether(100));
         await principalToken.setBalance.sendTransactionAsync(CREDITOR_3, Units.ether(100));
 
-        await principalToken.approve.sendTransactionAsync(tokenTransferProxy.address,
-            Units.ether(100), { from: DEBTOR_1 });
-        await principalToken.approve.sendTransactionAsync(tokenTransferProxy.address,
-            Units.ether(100), { from: DEBTOR_2 });
-        await principalToken.approve.sendTransactionAsync(tokenTransferProxy.address,
-            Units.ether(100), { from: DEBTOR_3 });
-
-        await principalToken.approve.sendTransactionAsync(tokenTransferProxy.address,
-            Units.ether(100), { from: CREDITOR_1 });
-        await principalToken.approve.sendTransactionAsync(tokenTransferProxy.address,
-            Units.ether(100), { from: CREDITOR_2 });
-        await principalToken.approve.sendTransactionAsync(tokenTransferProxy.address,
-            Units.ether(100), { from: CREDITOR_3 });
-
-        const debtRegistry = await DebtRegistryContract.deployed(web3, TX_DEFAULTS);
-        repaymentRouter = await RepaymentRouterContract.deployed(web3, TX_DEFAULTS);
-
-        const termsContractTruffle = await simpleInterestTermsContract.new(
-            debtRegistry.address,
-            principalToken.address,
-            repaymentRouter.address,
+        await principalToken.approve.sendTransactionAsync(
+            tokenTransferProxy.address,
+            Units.ether(100),
+            { from: DEBTOR_1 },
+        );
+        await principalToken.approve.sendTransactionAsync(
+            tokenTransferProxy.address,
+            Units.ether(100),
+            { from: DEBTOR_2 },
+        );
+        await principalToken.approve.sendTransactionAsync(
+            tokenTransferProxy.address,
+            Units.ether(100),
+            { from: DEBTOR_3 },
         );
 
-        // The typings we use ingest vanilla Web3 contracts, so we convert the
-        // contract instance deployed by truffle into a Web3 contract instance
-        const termsContractWeb3Contract =
-            web3.eth.contract(simpleInterestTermsContract.abi).at(termsContractTruffle.address);
+        await principalToken.approve.sendTransactionAsync(
+            tokenTransferProxy.address,
+            Units.ether(100),
+            { from: CREDITOR_1 },
+        );
+        await principalToken.approve.sendTransactionAsync(
+            tokenTransferProxy.address,
+            Units.ether(100),
+            { from: CREDITOR_2 },
+        );
+        await principalToken.approve.sendTransactionAsync(
+            tokenTransferProxy.address,
+            Units.ether(100),
+            { from: CREDITOR_3 },
+        );
 
-        termsContract = new SimpleInterestTermsContractContract(termsContractWeb3Contract, TX_DEFAULTS);
-
-        await tokenTransferProxy.addAuthorizedTransferAgent.sendTransactionAsync(repaymentRouter.address);
-
-        const termLengthInBlocks = 43200;
-        const principalPlusInterest = Units.ether(1.1);
-
-        const uint16PrincipalPlusInterest = leftPad(web3.toHex(principalPlusInterest).substr(2), 32, "0");
-        const uint16TermLength = leftPad(web3.toHex(termLengthInBlocks).substr(2), 32, "0");
-
-        const termsContractParameters = "0x" + uint16PrincipalPlusInterest + uint16TermLength;
+        const termsContractParameters = SimpleInterestParameters.pack({
+            principalTokenIndex: dummyREPTokenIndex, // Our migrations set REP up to be at index 0 of the registry
+            principalAmount: Units.ether(1), // principal of 1 ether
+            interestRateFixedPoint: Units.interestRateFixedPoint(2.5), // interest rate of 2.5%
+            amortizationUnitType: new BigNumber(1), // The amortization unit type (weekly)
+            termLengthUnits: new BigNumber(4), // Term length in amortization units.
+        });
 
         const defaultOrderParams = {
             creditorFee: Units.ether(0),
@@ -136,7 +136,11 @@ contract("Collateralized Debt Obligation", async (ACCOUNTS) => {
             debtTokenContract: debtToken.address,
             debtor: DEBTOR_1,
             debtorFee: Units.ether(0),
-            expirationTimestampInSec: new BigNumber(moment().add(1, "days").unix()),
+            expirationTimestampInSec: new BigNumber(
+                moment()
+                    .add(1, "days")
+                    .unix(),
+            ),
             issuanceVersion: repaymentRouter.address,
             orderSignatories: { debtor: DEBTOR_1, creditor: CREDITOR_1 },
             principalAmount: Units.ether(1),
@@ -147,7 +151,7 @@ contract("Collateralized Debt Obligation", async (ACCOUNTS) => {
             termsContractParameters,
             underwriter: NULL_ADDRESS,
             underwriterFee: Units.ether(0),
-            underwriterRiskRating: Units.percent(0),
+            underwriterRiskRating: Units.underwriterRiskRatingFixedPoint(0),
         };
 
         orderFactory = new DebtOrderFactory(defaultOrderParams);
@@ -191,8 +195,9 @@ contract("Collateralized Debt Obligation", async (ACCOUNTS) => {
         });
 
         it("should issue creditor a unique debt token", async () => {
-            await expect(debtToken.ownerOf.callAsync(new BigNumber(agreementId)))
-                .to.eventually.equal(CREDITOR_2);
+            await expect(
+                debtToken.ownerOf.callAsync(new BigNumber(agreementId)),
+            ).to.eventually.equal(CREDITOR_2);
         });
 
         it("should allow debtor to make repayment", async () => {
@@ -205,8 +210,9 @@ contract("Collateralized Debt Obligation", async (ACCOUNTS) => {
                 { from: DEBTOR_2 },
             );
 
-            await expect(principalToken.balanceOf.callAsync(CREDITOR_2))
-                .to.eventually.bignumber.equal(creditorBalanceBefore.plus(Units.ether(1)));
+            await expect(
+                principalToken.balanceOf.callAsync(CREDITOR_2),
+            ).to.eventually.bignumber.equal(creditorBalanceBefore.plus(Units.ether(1)));
         });
 
         it("should allow creditor to transfer debt token to different address", async () => {
@@ -216,8 +222,9 @@ contract("Collateralized Debt Obligation", async (ACCOUNTS) => {
                 { from: CREDITOR_2 },
             );
 
-            await expect(debtToken.ownerOf.callAsync(new BigNumber(agreementId)))
-                .to.eventually.equal(CREDITOR_1);
+            await expect(
+                debtToken.ownerOf.callAsync(new BigNumber(agreementId)),
+            ).to.eventually.equal(CREDITOR_1);
         });
     });
 });
